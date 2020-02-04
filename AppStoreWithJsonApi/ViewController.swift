e0b285e7942545f58f3a87f9d3f23048323acbd4
RJ Hrabowskie <rjhrabowskie@gmail.com>
2020-02-04 07:58:00 -0500

    Initial Commit

---
+++ AppStoreWithJsonApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreWithJsonApi
+//
+//  Created by Hrabowskie, Rj on 2/4/20.
+//  Copyright © 2020 Hrabowskie, Rj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
