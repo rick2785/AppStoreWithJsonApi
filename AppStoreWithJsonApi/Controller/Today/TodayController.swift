492c5310bb3d198de4da1d50224cc56253262728
rick2785 <rjhrabowskie@gmail.com>
2020-02-15 11:30:50 -0500

    47

--- AppStoreWithJsonApi/Controller/Today/TodayController.swift
+++ AppStoreWithJsonApi/Controller/Today/TodayController.swift
@@ -15,7 +15,7 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout, U
         let aiv = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
         aiv.color = .darkGray
         aiv.startAnimating()
-        aiv.hidesWhenStopped = false
+        aiv.hidesWhenStopped = true
         return aiv
     }()
     
@@ -110,16 +110,33 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout, U
         return true
     }
     
+    var appFullscreenBeginOffset: CGFloat = 0
+    
     @objc fileprivate func handleDrag(gesture: UIPanGestureRecognizer) {
-        let translationY = gesture.translation(in: appFullscreenController.view).y
+        if gesture.state == .began {
+            appFullscreenBeginOffset = appFullscreenController.tableView.contentOffset.y
+        }
         
+        if appFullscreenController.tableView.contentOffset.y > 0 {
+            return
+        }
+        
+        let translationY = gesture.translation(in: appFullscreenController.view).y
         if gesture.state == .changed {
-            let scale = 1 - translationY / 1000
-            
-            let transform: CGAffineTransform = .init(scaleX: scale, y: scale)
-            self.appFullscreenController.view.transform = transform
+            if translationY > 0 {
+                
+                let trueOffset = translationY - appFullscreenBeginOffset
+                var scale = 1 - trueOffset / 1000
+                scale = min(1, scale)
+                scale = max(0.5, scale)
+                
+                let transform: CGAffineTransform = .init(scaleX: scale, y: scale)
+                self.appFullscreenController.view.transform = transform
+            }
         } else if gesture.state == .ended {
-            handleAppFullscreenDismissal()
+            if translationY > 0 {
+                handleAppFullscreenDismissal()
+            }
         }
     }
     
@@ -191,6 +208,7 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout, U
             self.tabBarController?.tabBar.transform = .identity
             
             guard let cell = self.appFullscreenController.tableView.cellForRow(at: [0, 0]) as? AppFullscreenHeaderCell else { return }
+            self.appFullscreenController.closeButton.alpha = 0
             cell.todayCell.topConstraint.constant = 24
             cell.layoutIfNeeded()
         }, completion: { _ in
---
+++ AppStoreWithJsonApi/Views/Music/MusicLoadingFooter.swift
@@ -0,0 +1,32 @@
+//
+//  MusicLoadingFooter.swift
+//  AppStoreWithJsonApi
+//
+//  Created by RJ Hrabowskie on 2/15/20.
+//  Copyright © 2020 Hrabowskie, Rj. All rights reserved.
+//
+
+import UIKit
+
+class MusicLoadingFooter: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let aiv = UIActivityIndicatorView(style: .whiteLarge)
+        aiv.color = .darkGray
+        aiv.startAnimating()
+        
+        let label = UILabel(text: "Loading more...", font: .systemFont(ofSize: 16))
+        label.textAlignment = .center
+        let stackView = VerticalStackView(arrangedSubviews: [
+            aiv, label
+        ], spacing: 8)
+        
+        addSubview(stackView)
+        stackView.centerInSuperview(size: .init(width: 200, height: 0))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
